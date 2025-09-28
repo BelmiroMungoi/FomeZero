58b16aa681b8c14d6a8b9961ac56073849ee8b14
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-28 16:18:01 +0200

    Refactoring the Profile response dto

--- src/main/java/com/bbm/fomezero/dto/response/ProfileResponse.java
+++
@@ -1,6 +0,0 @@
-package com.bbm.fomezero.dto.response;
-
-public class ProfileResponse {
-
-    private String phoneNumber;
-}
---
+++ src/main/java/com/bbm/fomezero/dto/response/ProfileResponseDTO.java
@@ -0,0 +1,11 @@
+package com.bbm.fomezero.dto.response;
+
+import lombok.Data;
+
+@Data
+public class ProfileResponseDTO {
+
+    private Long id;
+    private String phoneNumber;
+    private String avatarUrl;
+}
