b883cafc84e841e5ad6f6590fd87ed3e4ef34fd7
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-28 16:21:21 +0200

    Changing the time type

--- src/main/java/com/bbm/fomezero/exception/handler/StandardErrorResponse.java
+++ src/main/java/com/bbm/fomezero/exception/handler/StandardErrorResponse.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class StandardErrorResponse {
 
     private int code;
     private HttpStatus status;
-    private OffsetDateTime time;
+    private String time;
     private String title;
     private String path;
     private List<ValidationError> fields;
