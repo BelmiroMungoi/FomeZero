dfe52cd0b697095827a2820e747b5327e2cea6c9
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-28 16:25:05 +0200

    Adding the method to get all driver and a specific driver by id

--- src/main/java/com/bbm/fomezero/service/DriverService.java
+++ src/main/java/com/bbm/fomezero/service/DriverService.java
@@ -2,9 +2,16 @@ package com.bbm.fomezero.service;
 
 import com.bbm.fomezero.dto.request.UserRequest;
 import com.bbm.fomezero.dto.response.AppResponse;
+import com.bbm.fomezero.dto.response.DriverResponseDTO;
 import com.bbm.fomezero.model.Driver;
+
+import java.util.List;
 
 public interface DriverService {
 
     AppResponse registerDriver(UserRequest userRequest);
+
+    List<DriverResponseDTO> getAllDrivers();
+
+    DriverResponseDTO getDriverById(Long id);
 }
--- src/main/java/com/bbm/fomezero/service/impl/DriverServiceImpl.java
+++ src/main/java/com/bbm/fomezero/service/impl/DriverServiceImpl.java
@@ -2,16 +2,21 @@ package com.bbm.fomezero.service.impl;
 
 import com.bbm.fomezero.dto.request.UserRequest;
 import com.bbm.fomezero.dto.response.AppResponse;
+import com.bbm.fomezero.dto.response.DriverResponseDTO;
 import com.bbm.fomezero.exception.ConflictException;
+import com.bbm.fomezero.exception.ResourceNotFoundException;
+import com.bbm.fomezero.mapper.EntityConverter;
 import com.bbm.fomezero.model.Driver;
 import com.bbm.fomezero.repository.DriverRepository;
 import com.bbm.fomezero.service.DriverService;
 import com.bbm.fomezero.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -21,6 +26,7 @@ public class DriverServiceImpl implements DriverService {
 
     private final UserService userService;
     private final DriverRepository driverRepository;
+    private final EntityConverter<Driver, DriverResponseDTO> driverConverter;
 
     @Override
     @Transactional
@@ -48,5 +54,20 @@ public class DriverServiceImpl implements DriverService {
                 .description("Welcome, " + user.getFullName() + "!")
                 .createdAt(LocalDateTime.now())
                 .build();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<DriverResponseDTO> getAllDrivers() {
+        return driverConverter.mapEntityToDtoList(driverRepository.findAll(), DriverResponseDTO.class);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public DriverResponseDTO getDriverById(Long id) {
+        var driver = driverRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Driver not found!"));
+
+        return driverConverter.mapEntityToDto(driver, DriverResponseDTO.class);
     }
 }
