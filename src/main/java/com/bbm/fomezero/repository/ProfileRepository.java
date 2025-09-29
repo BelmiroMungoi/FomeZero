b7b8aff88eea19f0afaa2712f807fae7c8fbd7bb
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-29 23:23:40 +0200

    Finding the user profile

--- src/main/java/com/bbm/fomezero/repository/ProfileRepository.java
+++ src/main/java/com/bbm/fomezero/repository/ProfileRepository.java
@@ -1,9 +1,14 @@
 package com.bbm.fomezero.repository;
 
 import com.bbm.fomezero.model.Profile;
+import com.bbm.fomezero.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findByUser(User user);
 }
