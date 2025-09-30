6725aac77fab71701bb08497604fe3148dc1445d
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-30 13:40:04 +0200

    Adding the address repository

---
+++ src/main/java/com/bbm/fomezero/repository/AddressRepository.java
@@ -0,0 +1,13 @@
+package com.bbm.fomezero.repository;
+
+import com.bbm.fomezero.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<Address> findAllByUserId(Long userId);
+}
