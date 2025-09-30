8563da7e3230ab09cd0c03c4eaab5ffa0a2a9a2d
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-09-30 22:44:26 +0200

    Adding methods to search the restaurant and get the restaurant by the owner

--- src/main/java/com/bbm/fomezero/repository/RestaurantRepository.java
+++ src/main/java/com/bbm/fomezero/repository/RestaurantRepository.java
@@ -2,8 +2,20 @@ package com.bbm.fomezero.repository;
 
 import com.bbm.fomezero.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("SELECT r FROM Restaurant r WHERE lower(r.name) LIKE lower(concat('%', :keyword, '%'))" +
+            "OR lower(r.cuisineType) LIKE lower(concat('%', :keyword, '%'))" +
+            "OR lower(r.description) LIKE lower(concat('%', :keyword, '%'))" +
+            "OR lower(r.address.city) LIKE lower(concat('%',:keyword, '%'))" +
+            "OR lower(r.address.province) LIKE lower(concat('%',:keyword, '%'))")
+    List<Restaurant> searchRestaurant(String keyword);
+
+    Restaurant findByOwnerId(Long id);
 }
