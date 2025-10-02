80fde688b788867aee2274c8fea5e805f0183a61
Belmiro Mungoi <belmiromungoi@gmail.com>
2025-10-02 22:09:38 +0200

    Adding the methods to find and search restaurant

--- src/main/java/com/bbm/fomezero/controller/RestaurantController.java
+++ src/main/java/com/bbm/fomezero/controller/RestaurantController.java
@@ -2,15 +2,15 @@ package com.bbm.fomezero.controller;
 
 import com.bbm.fomezero.dto.request.RestaurantRequestDTO;
 import com.bbm.fomezero.dto.response.AppResponse;
+import com.bbm.fomezero.dto.response.RestaurantResponseDTO;
 import com.bbm.fomezero.service.RestaurantService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +23,25 @@ public class RestaurantController {
     @PostMapping("/")
     public ResponseEntity<AppResponse> createRestaurant(@RequestBody RestaurantRequestDTO restaurantRequest) {
         return new ResponseEntity<>(restaurantService.createRestaurant(restaurantRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RestaurantResponseDTO>> getAllRestaurants() {
+        return ResponseEntity.ok(restaurantService.getAllRestaurants());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<RestaurantResponseDTO>> searchRestaurants(@RequestParam("keyword") String keyword) {
+        return ResponseEntity.ok(restaurantService.searchRestaurants(keyword));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantResponseDTO> findRestaurantById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(restaurantService.findRestaurantById(id));
+    }
+
+    @GetMapping("/owner")
+    public ResponseEntity<RestaurantResponseDTO> getRestaurantByOwnerId(@RequestParam("id") Long id) {
+        return ResponseEntity.ok(restaurantService.getRestaurantByOwnerId(id));
     }
 }
